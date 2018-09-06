31e5e2e88557dc8e4dbaacb57c25026b959a7be7
SeiF Sameh <seif.sameh@udacity.com>
2018-09-06 23:43:34 +0200

    move /predict to its own module

--- src/predictmod/app.py
+++ src/predictmod/app.py
@@ -18,34 +18,4 @@ def hello_world():
     return 'prediction-module: OK'
 
 
-@app.route('/predict')
-def predict():
-    start_time = flask.request.args.get('start_time')
-    end_time = flask.request.args.get('end_time')
-    if start_time is None:
-        return flask.jsonify({'error': 'Missing query param: start_time'})
-    if end_time is None:
-        return flask.jsonify({'error': 'Missing query param: end_time'})
-
-    # TODO: Validate params are in expected format
-
-    # Granularity by default is 1 hour
-    model = ArimaModel()
-    test, predictions = model.train_model_on_batch(model.training_data, model.testing_data)
-
-    to_forecast = 0
-    start_time = datetime.datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%SZ')
-    end_time = datetime.datetime.strptime(end_time, '%Y-%m-%dT%H:%M:%SZ')
-    temp_time = start_time
-    while temp_time <= end_time:
-        to_forecast += 1
-        temp_time += datetime.timedelta(hours=1)
-
-    # For now let's assume that the start_time will always be directly after the last training data
-    # predictions = model.forecast(to_forecast)
-    ret = []
-    t = start_time
-    for p in predictions:
-        ret.append({'time-stamp': t.strftime('%Y-%m-%dT%H:%M:%SZ'), 'requests': p[0]})
-        t += datetime.timedelta(hours=1)
-    return flask.jsonify({'id': 1, 'values': ret})
+import apis.predict
