2e26643d71640d20c30d0fb5bcdc248f76eacaa9
SeiF Sameh <seif.sameh@udacity.com>
2018-09-11 22:44:06 +0200

    /predict returns predictions for requested timeframe

--- src/predictmod/apis/predict.py
+++ src/predictmod/apis/predict.py
@@ -1,10 +1,12 @@
 from predictmod.app import api
 from predictmod.forecast_models.arima import ArimaModel
+from predictmod import db_helper
 from predictmod import utils
 import flask_restful as rest
 import flask
 import datetime
 import sys
+
 
 class Predict(rest.Resource):
 
@@ -46,20 +48,40 @@ class Predict(rest.Resource):
         Steps:
         1- Check if there's a saved model
         2- Load saved model and do predictions
-        3- If no model is saved -> create new model, train and predict. 
+        3- If no model is saved -> create new model, train and predict.
         '''
-        active_model = utils.get_active_model()
+        active_model = db_helper.get_active_model()
         print >> sys.stderr, "Active Model: {}".format(active_model)
         if active_model:
             model = ArimaModel(load_id=active_model)
-            predictions = model.forecast(10)
-            print >> sys.stderr, predictions
+            model_attributes = db_helper.get_model_by_id(active_model)
+
+            # K is the number of units (typically hours) we need to forecast
+            # S is the number of units to skip from input_end to start_time
+            K = utils.get_time_difference(model_attributes['input_end'], end_time)
+            S = utils.get_time_difference(model_attributes['input_end'], start_time)
+
+            if S > K:
+                return flask.jsonify(
+                    {
+                        'status': 400,
+                        'message': "Bad Request, start_time should be before end_time"
+                    }
+                )
+
+            # start_time is the first timestamp to be sent in the response
+            start_time = datetime.datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%S')
+            # end_time is the last timestamp to be send in the response
+            end_time = datetime.datetime.strptime(end_time, '%Y-%m-%dT%H:%M:%S')
+
+            predictions = model.forecast(K)
+
+            # Discard first S predictions since they are not required in the response
+            predictions_trimmed = predictions[S:]
             ret = []
-            start_time = datetime.datetime.strptime(start_time, '%Y-%m-%dT%H:%M:%SZ')
-            end_time = datetime.datetime.strptime(end_time, '%Y-%m-%dT%H:%M:%SZ')
             t = start_time
-            for p in predictions:
-                ret.append({'time-stamp': t.strftime('%Y-%m-%dT%H:%M:%SZ'), 'requests': p})
+            for p in predictions_trimmed:
+                ret.append({'time-stamp': t.strftime('%Y-%m-%dT%H:%M:%S'), 'requests': p})
                 t += datetime.timedelta(hours=1)
             return flask.jsonify({'id': 1, 'values': ret})
         else:
