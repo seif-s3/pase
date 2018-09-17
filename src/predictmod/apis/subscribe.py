e9c4f75ccad7adb8fb97a29e4909773eb44a86bd
SeiF Sameh <seif.sameh@udacity.com>
2018-09-17 23:09:11 +0200

    fix parsing error in test_sub endpoint

--- src/predictmod/apis/subscribe.py
+++ src/predictmod/apis/subscribe.py
@@ -108,7 +108,7 @@ class TestSubscribe(rest.Resource):
         url = subscriber['url']
 
         mock = []
-        ts = datetime.datetime.strptime(predictions['start_time'], '%Y-%m-%dT%H:%M:%S')
+        ts = datetime.datetime.strptime(predictions['start_time'], '%Y-%m-%dT%H:%M:%SZ')
         print >> sys.stderr, ts, type(ts)
         for p, t in zip(predictions['values'], thresholds):
             mock.append(
