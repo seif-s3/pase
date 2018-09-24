b99ee71f1fd7444a76470b580dc9c00bb19d47d1
SeiF Sameh <seif.sameh@udacity.com>
2018-09-24 16:09:52 +0200

    add min/max and scaling samples

--- ts_gen.py
+++ ts_gen.py
@@ -1,6 +1,7 @@
 import argparse
 import timesynth as ts
 from datetime import datetime, timedelta
+from scipy.interpolate import interp1d
 
 
 def makeParser():
@@ -9,34 +10,44 @@ def makeParser():
     parser.add_argument(
         '-func', help='Specify a function to ping according to')
     parser.add_argument('-start', help='Start time in format YYYY-MM-DDTHH:mm:ssZ')
-    parser.add_argument('-steps', help='Number of samples to generate')
+    parser.add_argument('-steps', help='Number of samples to generate', type=int)
     parser.add_argument('-g', help='Granularity: default is hours')
-    return parser
+    parser.add_argument('-max', help='Max range for values', type=int)
+    parser.add_argument('-min', help='Max range for values', type=int)
+    parser.add_argument('-noise', help='Add noise?', type=bool)
+    return parser.parse_args()
 
 
-def ar(steps, scale):
+def ar(steps, min, max):
     # Initializing TimeSampler
-    steps = int(steps)
     time_sampler = ts.TimeSampler(stop_time=steps)
     # Sampling regular time samples
     regular_time_samples = time_sampler.sample_regular_time(num_points=steps)
-
     # Initializing AR(2) model
     ar_p = ts.signals.AutoRegressive(ar_param=[1.5, -0.75])
     ar_p_series = ts.TimeSeries(signal_generator=ar_p)
-    samples = ar_p_series.sample(regular_time_samples)
-    return samples[0]
+    samples, signals, errors = ar_p_series.sample(regular_time_samples)
+    interpolator = interp1d([samples.min(), samples.max()], [min, max])
+    ret = []
+    for s in samples:
+        ret.append(int(interpolator(s)))
+    return ret
 
 
 def main():
-    args = makeParser().parse_args()
+    args = makeParser()
     try:
         start = datetime.strptime(args.start, '%Y-%m-%dT%H:%M:%SZ')
     except:
         print("Cannot parse start date. Must be in format %Y-%m-%dT%H:%M:%SZ")
+        return
+
+    if not args.min or not args.max:
+        print("Error: -min and -max are required arguments")
+        return
 
     if args.func == 'ar':
-        samples = ar(args.steps, args.scale)
+        samples = ar(args.steps, args.min, args.max)
 
     t = start
     print("timestamp,value")
