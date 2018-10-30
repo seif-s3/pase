e605e13cdf11dfcafcf6c9a0e29783bbfd698621
SeiF Sameh <seif.sameh@udacity.com>
2018-10-30 23:43:01 +0100

    test config param

--- src/predictmod/apis/config.py
+++ src/predictmod/apis/config.py
@@ -6,27 +6,29 @@ from flask import jsonify
 from predictmod.app import api, app
 
 
-def makeConfigParser(for_update=False):
+def makeInfluxConfigParser(for_update=False):
     parser = reqparse.RequestParser(trim=True)
     parser.add_argument('influx_host', required=True, nullable=False)
     parser.add_argument('influx_db', required=True, nullable=False)
     parser.add_argument('influx_user', required=True, nullable=False)
     parser.add_argument('influx_pass', required=True, nullable=False)
-    parser.add_argument('test', nullable=False)
+    return parser
+
+
+def makeTestConfigParser(for_update=False):
+    parser = reqparse.RequestParser(trim=True)
+    parser.add_argument('test', required=True, nullable=False)
     return parser
 
 
 class InfluxConfig(rest.Resource):
     def post(self):
-        args = makeConfigParser().parse_args()
+        args = makeInfluxConfigParser().parse_args()
         try:
             app.config['INFLUX_HOST'] = args.get('influx_host')
             app.config['INFLUX_DB'] = args.get('influx_db')
             app.config['INFLUX_USER'] = args.get('influx_user')
             app.config['INFLUX_PASS'] = args.get('influx_pass')
-            # Restart gunicorn gracefully to apply changes
-            # http://docs.gunicorn.org/en/stable/faq.html
-            os.system('kill -HUP masterpid')
         except Exception as e:
             print >> sys.stderr, 'Error reloading server to update config!\n', e
 
@@ -40,4 +42,24 @@ class InfluxConfig(rest.Resource):
             }
         )
 
+
+class TestConfig(rest.Resource):
+    def post(self):
+        args = makeTestConfigParser().parse_args()
+        try:
+            app.config['TEST_CONFIG'] = args.get('test')
+            # Restart gunicorn gracefully to apply changes
+            # http://docs.gunicorn.org/en/stable/faq.html
+            print >> sys.stderr, "Set Test Config... Restarting!"
+        except Exception as e:
+            print >> sys.stderr, 'Error reloading server to update config!\n', e
+
+    def get(self):
+        return jsonify(
+            {
+                'test_config': app.config.get('TEST_CONFIG'),
+            }
+        )
+
 api.add_resource(InfluxConfig, '/config/influx')
+api.add_resource(TestConfig, '/config/test')
